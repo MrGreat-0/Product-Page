7a4900954456099b87458eb34f5762946b027149
MrGreat-0 <jagjeetsinghrana2@gmail.com>
2024-12-05 17:20:37 +0530

    third commit
    make more responsive .

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,9 +7,9 @@ const Card = ({ product }) => {
   return (
     <Link
       to={`/details/${id}`}
-      className="relative  text-zinc-200 h-auto w-[85%] sm:w-[45%] md:w-[30%] lg:w-[25%] xl:w-[20%] border rounded-xl p-3 flex flex-col items-center justify-center overflow-hidden"
+      className="relative  text-zinc-200 h-[50%] sm:h-[40%] md:h-[30%] lg:h-[35%] xl:h-[40%] 2xl:h-[50%] w-[95%] sm:w-[45%] md:w-[30%] lg:w-[30%] xl:w-[30%] 2xl:w-[20%] border rounded-xl p-3 flex flex-col items-center justify-center overflow-hidden"
     >
-      <div className="relative h-[70%] w-full border rounded-md mb-2 overflow-hidden ">
+      <div className="relative h-[80%] w-full border rounded-md mb-2 overflow-hidden ">
         <img
           className="w-full h-full object-cover hover:scale-105 bg-white"
           src={image}
